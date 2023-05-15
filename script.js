9d080e338396c6f63bfa1877b26567bc8e818083
MariaEvpalova <mashayevpalova@gmail.com>
2023-05-16 00:58:20 +0530

    Add read/not read button functionality - Fix bug of not closing form after adding a new book

--- script.js
+++ script.js
@@ -15,6 +15,13 @@ let myLibrary = [];
 
 const mainSection = document.getElementById('main-section');
 
+function changeReadButton(event) {
+    if (event.target.src.includes('undone'))
+        event.target.src = event.target.src.replace('undone', 'done');
+    else
+        event.target.src = event.target.src.replace('done', 'undone')
+}
+
 function displaySingleBook(book) {
     const htmlCode = `
     <div class="book">
@@ -24,11 +31,23 @@ function displaySingleBook(book) {
         <h2>${book.author}</h2>
         </div>
         <div class="buttons">
-        <button><img src="icons/done.svg" alt="done"></button>
+        <button><img src="icons/undone.svg" alt="done"></button>
         <button><img src="icons/delete.svg" alt="delete"></button>
         </div>
     </div>`;
-    mainSection.innerHTML += htmlCode;
+
+    let container = document.createElement('div');
+    container.innerHTML = htmlCode;
+
+    const containerChildren = container.children;
+    const readButton = containerChildren[0].querySelector('button');
+    const readIcon = readButton.querySelector('img');
+    readIcon.src = book.read ? 'icons/done.svg' : 'icons/undone.svg';
+
+    mainSection.appendChild(container);
+
+    readButton.addEventListener('click', changeReadButton);
+
 }
 
 function addBook(book) {
@@ -52,14 +71,15 @@ showFormButton.addEventListener('click', function() {
 
 const form = formContainer.querySelector('form');
 form.addEventListener('submit', function(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  const title = document.getElementById('title').value;
-  const author = document.getElementById('author').value;
-  const pages = document.getElementById('pages').value;
-  const read = document.getElementById('read').checked;
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const read = document.getElementById('read').checked;
 
-  addBook(new Book(title, author, pages, read));
+    addBook(new Book(title, author, pages, read));
 
-  form.reset();
+    form.reset();
+    formContainer.classList.toggle('visible');
 });
